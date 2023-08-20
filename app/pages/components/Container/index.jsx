cf7fec65b3474f100f0e30365508f4663a16f115
Andre Fabiano <andregtalima@gmail.com>
2023-08-20 12:35:01 -0300

    commit atualization main

--- app/pages/components/Container/index.jsx
+++ app/pages/components/Container/index.jsx
@@ -1,9 +1,16 @@
+import './Container.scss'
+
 const Container = () => {
     return (
-        <section>
-            <div>
-                <img src="./image/image-web-3-mobile.jpg" alt="" />
+        <section className='section-one'>
+            <div className='section-one-img'>
+                <img src="./image/image-web-3-mobile.jpg" alt="First image" />
             </div>
+            <div className='section-one-text'>
+                <h1>The Bright Future of Web 3.0?</h1>
+                <p>We dive into the next evolution of the web that claims to put the power of the platforms back into the hands of the people. But is it really fultilling its promise?</p>
+            </div>
+            <button className='sectio-one-btn'>Read More</button>
         </section>
     )
 }

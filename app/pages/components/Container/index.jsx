e0bcf60bd601df322e6a51d39b8583ac3f0f7d87
Andre Fabiano <andregtalima@gmail.com>
2023-08-20 23:09:58 -0300

    Atualization main section-one

--- app/pages/components/Container/index.jsx
+++ app/pages/components/Container/index.jsx
@@ -2,16 +2,18 @@ import './Container.scss'
 
 const Container = () => {
     return (
-        <section className='section-one'>
-            <div className='section-one-img'>
-                <img src="./image/image-web-3-mobile.jpg" alt="First image" />
+        <>
+            <section className='section-one'>
+                <img className='section-one-img' src="./image/image-web-3-mobile.jpg" alt="First image" />
+                <div className='section-one-txt'>
+                    <h1 className='text'>The Bright Future of Web 3.0?</h1>
+                    <p className='text'>We dive into the next evolution of the web that claims to put the power of the platforms back into the hands of the people. But is it really fultilling its promise?</p>
+                </div>
+            </section>
+            <div className='sectio-one-btn'>
+                <button>Read More</button>
             </div>
-            <div className='section-one-text'>
-                <h1>The Bright Future of Web 3.0?</h1>
-                <p>We dive into the next evolution of the web that claims to put the power of the platforms back into the hands of the people. But is it really fultilling its promise?</p>
-            </div>
-            <button className='sectio-one-btn'>Read More</button>
-        </section>
+        </>
     )
 }
 

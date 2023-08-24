44327babe64584a0ddf0a50a15c4ebfa385d0a97
Andre Fabiano <andregtalima@gmail.com>
2023-08-23 23:14:33 -0300

    container actualization

--- app/pages/components/Container/index.jsx
+++ app/pages/components/Container/index.jsx
@@ -13,18 +13,18 @@ const Container = () => {
                         <button>Read More</button>
                     </div>
                 </div>
-                <div className="section-div__two">
-                    <h1>New</h1>
-                    <div>
-                        <h2>Hydrogen VS Electric Cars</h2>
+                <div className="section-div__two">                   
+                    <div className='div-txt__two'>
+                        <h2>New</h2>
+                        <h3>Hydrogen VS Electric Cars</h3>
                         <p>Will hydrogen-fueled cars ever catch up to EVs?</p>
                     </div>
-                    <div>
-                        <h2>The Downsides of AI Artistry</h2>
+                    <div className='div-txt__two'>
+                        <h3>The Downsides of AI Artistry</h3>
                         <p>What are possible adverse effects of on-demand AI image generation?</p>
                     </div>
-                    <div>
-                        <h2>Is VC Funding Drying Up?</h2>
+                    <div className='div-txt__two'>
+                        <h3>Is VC Funding Drying Up?</h3>
                         <p>Private funding by VC firms is down 50% YOY. We take a look at what that means.</p>
                     </div>
                 </div>
